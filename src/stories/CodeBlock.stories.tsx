5318f66f0494846e61f89d80d15f9d65bcd2c1fc
Captain Codex <legendofthesavage@gmail.com>
2019-02-28 01:29:01 +0800

    Removed css line from storybook CodeBlock.

--- src/stories/CodeBlock.stories.tsx
+++ src/stories/CodeBlock.stories.tsx
@@ -11,7 +11,6 @@ import { action } from '@storybook/addon-actions';
 import { object, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import { CodeBlock } from '../components/code-block/CodeBlock';
-import '../assets/global.scss';
 import _ from 'lodash';
 
 const code = 'the code for this story';
